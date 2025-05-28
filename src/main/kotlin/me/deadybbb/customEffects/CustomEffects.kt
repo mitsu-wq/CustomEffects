7ca6a7b1e38cd512067cafb53225c947edb0c0c4
mitsu-wq <bmitsu779@gmail.com>
2025-05-28 12:23:23 +0700

    Initial commit

---
+++ src/main/kotlin/me/deadybbb/customEffects/CustomEffects.kt
@@ -0,0 +1,14 @@
+package me.deadybbb.customEffects
+
+import org.bukkit.plugin.java.JavaPlugin
+
+class CustomEffects : JavaPlugin() {
+
+    override fun onEnable() {
+        // Plugin startup logic
+    }
+
+    override fun onDisable() {
+        // Plugin shutdown logic
+    }
+}
